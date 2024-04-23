26d5183a9b5cc1ca974b416ee43418ac7cd16f48
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2024-04-23 22:08:43 +0000

    adapt to apollo

--- anykernel.sh
+++ anykernel.sh
@@ -4,24 +4,24 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Redline
+kernel.string=Pharoh
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=alioth
-device.name2=aliothin
+device.name1=apollo
+device.name2=apollon
+device.name3=apollopro
 supported.versions=11 - 14
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=1;
+block=/dev/block/by-name/boot;
+is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
@@ -46,13 +46,13 @@ write_boot;
 
 
 # shell variables
-block=vendor_boot;
-is_slot_device=1;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
+#block=/dev/block/by-name/boot;
+#is_slot_device=0;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
 
 # reset for vendor_boot patching
-reset_ak;
+#reset_ak;
 
 
 ## AnyKernel vendor_boot install
