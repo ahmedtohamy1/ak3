4e4934a9345d7652b21fa41f91473da3b4c8aabd
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2025-02-23 22:26:58 +0000

    aa

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=MiNaZuKi kernel by amack @ xda-developers
+kernel.string=agmad kernel by Ahmed Tohmay @ telegram
 do.devicecheck=1
 do.modules=0
 do.systemless=1
